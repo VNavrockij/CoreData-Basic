0ba16ac649795b0918c852106bf9a8adf8910c0d
VNavrockij <vitalik.nav@gmail.com>
2022-10-11 16:30:53 +0300

    Initial Commit

---
+++ CoreData_Basic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData_Basic
+//
+//  Created by Vitalii Navrotskyi on 11.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
