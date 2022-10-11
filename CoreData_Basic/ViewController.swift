4db1c74fe27c1be39d9014ca7f1f604476e3a3a8
VNavrockij <vitalik.nav@gmail.com>
2022-10-11 18:16:44 +0300

    add: reference to AppDelegate, create context, describe the entity, set attributes

--- CoreData_Basic/ViewController.swift
+++ CoreData_Basic/ViewController.swift
@@ -6,12 +6,38 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // reference to AppDelegate
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        // create context
+        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        // describe the entity
+        let entityDescription = NSEntityDescription.entity(forEntityName: "Person", in: context)
+        
+        // create instance
+        let managedObject = NSManagedObject(entity: entityDescription!, insertInto: context)
+        
+        // set attributes
+        managedObject.setValue("Vasya", forKey: "name")
+        managedObject.setValue(18, forKey: "age")
+        
+        // unwrap context value form attributes
+        let name = managedObject.value(forKey: "name")
+        let age = managedObject.value(forKey: "age")
+        print("\(name) \(age)")
+        
+        // saving data
+        appDelegate.saveContext()
+        
     }
 
 
